e12d9ece87b42aae9a678d174d64b06d3bbe0824
MaddaGh <92437363+MaddaGh@users.noreply.github.com>
2025-08-08 12:59:45 +0200

    fixed }, at the end of csv entries

--- data/ACM/bib_to_csv.py
+++ data/ACM/bib_to_csv.py
@@ -38,7 +38,7 @@ def convert_bib_to_csv(bib_file):
     bib_dataframe = pd.DataFrame()
     bib_dictionary = {}
     count = 0
-    print(len(bib_dataframe))
+    # print(len(bib_dataframe))
     with open(bib_file, 'r', encoding='utf-8') as file:
         lines = file.readlines()
         for line in lines:
@@ -63,11 +63,13 @@ def convert_bib_to_csv(bib_file):
                 value = value.strip().strip('{}"')
                 # add the key-value pair to the dictionary
                 bib_dictionary[key] = value
-                print(bib_dictionary)
+                # print(bib_dictionary)
             else:
                 print('unexpected line format:', line)
                 continue
-        print(len(bib_dataframe), bib_dataframe)
+        # print(len(bib_dataframe), bib_dataframe)
+
+        bib_dataframe = bib_dataframe.applymap(lambda x: x.rstrip('},') if isinstance(x, str) and x.endswith('},') else x)
         bib_dataframe.to_csv(f"{bib_file[:-4]}.csv", index=False)  # save the dataframe to a csv file
 
 # script to add rows to the dataframe
@@ -78,4 +80,4 @@ def new_row_from_bib(dictionary, dataframe):
 # print(new_row_from_bib({'author' :  'Pang, Rock Yuren and Maheshwari, Rohit and Yu, Julie and Reinecke, Katharina','title' : 'Synthetic Conversation: How Computing Researchers Engage Multi-Perspective Dialogues to Brainstorm Societal Impacts','year' : '2025', 'isbn' : '9798400713958','publisher' : 'Association for Computing Machinery','address' : 'New York, NY, USA','url' : 'https://doi.org/10.1145/3706599.3719747','doi' : '10.1145/3706599.3719747','abstract' : 'There have been increasing calls for computing researchers to consider the negative societal impacts of their work. However, anticipating these impacts remains challenging, as computing researchers often do not have access to diverse perspectives. Here, we explore how computing researchers brainstorm the negative societal impacts of computing innovations using a multi-agent dialog prototype called Weaver as a probe that represents synthetic stakeholder conversations. Through think-aloud sessions and interviews with 12 participants, we evaluate how computing researchers perceive and engage with such a system, and whether they find it beneficial compared to using no such system or using ChatGPT. Our findings revealed that participants valued the conversations with the multi-agent system and considered societal impacts from new angles. Participants reported gaining insights and agency to reflect on issues rather than passively consuming pre-generated content. We discuss the findings, implications, and next steps for using a multi-agent system to brainstorm about the societal impacts of computing technologies.','booktitle' : 'Proceedings of the Extended Abstracts of the CHI Conference on Human Factors in Computing Systems','articleno' : '497','numpages' : '7','keywords' : 'Multi-agent systems, Brainstorming, Human-AI Interaction, LLM','location' : '','series' : 'CHI EA 25'}, pd.DataFrame()))
 # print(convert_bib_to_csv(bib_file))
 # bib_files = get_bib_file_path('RAW_DATA\CHI.bib')
-print(convert_bib_to_csv('RAW_DATA\CSCW.bib'))
+print(convert_bib_to_csv('path_to_bib_file'))
